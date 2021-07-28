3aae8f213b1eb5646e1b3f419431650cc81f9125
MECHREVO <dhg594690048@163.com>
2021-07-28 13:44:34 +0800

    three

--- pay-api/src/main/java/com/lola/digiccy/util/HttpUtil.java
+++ pay-api/src/main/java/com/lola/digiccy/util/HttpUtil.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.lola.digiccy.response.TopApiResponse;
 import okhttp3.*;
 import okhttp3.OkHttpClient;
-
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,8 +12,8 @@ import java.util.concurrent.TimeUnit;
 public class HttpUtil {
     private  static  final Integer SUCCESS=200;
     public static final MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
-    public static String callBack(String url, TopApiResponse.TradeNotify tradeNotify) throws Exception {
-        OkHttpClient   client = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).build();
+    public static String callBack(String url,  TopApiResponse.TradeNotify tradeNotify) throws Exception {
+        OkHttpClient client = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).build();
         String result = null;
         RequestBody requestBody = RequestBody.create(JSON.toJSONString(tradeNotify), mediaType);
         Request request = new Request.Builder().post(requestBody).url(url).build();
--- pay-api/target/classes/static/js/global.js
+++
@@ -1,5 +0,0 @@
-function  getBasePath(){
-    let path = window.location.pathname.split("/");
-    let basePath = path[0]+"/"+path[1]+"/";
-    return basePath;
-}
